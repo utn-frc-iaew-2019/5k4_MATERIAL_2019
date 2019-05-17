463a93972ac68e4df47ed37586f483b9e19df1b2
romeroruben <romeroa.ruben@gmail.com>
2019-05-17 16:16:03 -0300

    Add files via upload

---
+++ semana 8 - OAuth2/OAuth2.0_Google/IAEW_ApiRest_App/Controllers/CallBack.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IAEW_ApiRest_App.Controllers
+{
+    [Route("api/[controller]")]
+    [EnableCors("CorsPolicy")]
+    public class CallBack : ControllerBase
+    {
+ 
+        // GET: api/callback/3
+        [HttpGet("{code}")]
+        public IActionResult Get(int code)
+        {
+            return Ok();
+        }       
+    }
+}
