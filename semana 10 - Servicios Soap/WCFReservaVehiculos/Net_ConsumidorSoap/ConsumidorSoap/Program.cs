bc485fa20d07925dd8a9ce0cc7a2c0fc2734ff2f
romeroruben <romeroa.ruben@gmail.com>
2019-05-28 16:59:30 -0300

    Add files via upload

---
+++ semana 10 - Servicios Soap/WCFReservaVehiculos/Net_ConsumidorSoap/ConsumidorSoap/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsumidorSoap.ServiceReference1;
+
+namespace ConsumidorSoap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Iniciando Aplicación...");
+
+            var client = new HelloWorldClient();
+            var valor = client.sayHelloWorld("pepe");
+
+            Console.WriteLine(valor);
+
+            Console.ReadLine();
+
+        }
+    }
+}
