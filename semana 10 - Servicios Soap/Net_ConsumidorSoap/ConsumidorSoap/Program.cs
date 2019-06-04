f040950a20ec34afdf01a7db7cb8bde455f9aa77
romeroruben <romeroa.ruben@gmail.com>
2019-06-04 15:13:43 -0300

    Net_ConsumidorSoap

---
+++ semana 10 - Servicios Soap/Net_ConsumidorSoap/ConsumidorSoap/Program.cs
@@ -0,0 +1,36 @@
+﻿using ConsumidorSoap.ServiceReference1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsumidorSoap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Iniciando Aplicación...");
+
+            var client = new WCFReservaVehiculosClient();
+            var credentials = new Credentials();
+            credentials.UserName = "grupo_nro1";
+            credentials.Password = "AzArobmjV0";
+
+            var request = new ConsultarCiudadesRequest();
+            request.IdPais = 1;
+            var valor = client.ConsultarCiudades(credentials, request);
+
+            foreach(var ciudad in valor.Ciudades)
+            {
+                Console.WriteLine(ciudad.Nombre);
+
+            }
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
